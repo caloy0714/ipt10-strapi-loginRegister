60098b3acb0f7eb2a7f8999e4cfce3b870a5cc88
unknown <caloy0714@gmail.com>
2022-11-14 09:27:35 +0800

    first commit

---
+++ welcome-login.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    </head>
+<section class="vh-100" style="background-color: #eee;">
+  <div class="container h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-lg-12 col-xl-11">
+        <div class="card text-black" style="border-radius: 25px;">
+          <div class="card-body p-md-5">
+            <div class="row justify-content-center">
+              <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+			  	<div class=" text-center">
+					<h4>Hello and welcome <b><?php echo $_GET['username'];?></b>!</h4>	
+					<p>You are now logged in using your email <b><?php echo $_GET['email']; ?></b>.</p>
+				</div>
+                <img src="https://www.icr3ate.nl/wp-content/uploads/2020/11/Logo-Strapi.png"
+                  class="img-fluid" alt="Sample image">
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
